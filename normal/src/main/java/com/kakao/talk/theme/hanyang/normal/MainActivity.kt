4422281b0cfbd6e0151c901404faed9b8ce4b476
jeongin Lee <jil8885@hanyang.ac.kr>
2022-01-05 15:28:49 +0900

    Update Gradle files

--- normal/src/main/java/com/kakao/talk/theme/hanyang/normal/MainActivity.kt
+++ normal/src/main/java/com/kakao/talk/theme/hanyang/normal/MainActivity.kt
@@ -4,18 +4,17 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import kotlinx.android.synthetic.main.main_activity.*
+import android.widget.Button
 
-class MainActivity : Activity() {
+open class MainActivity : Activity() {
 
     companion object {
         private const val KAKAOTALK_SETTINGS_THEME_URI = "kakaotalk://settings/theme/"
         private const val MARKET_URI = "market://details?id="
-        private const val KAKAO_TALK_PACKAGE_NAME = "com.kakao.talk"
+        const val KAKAOTALK_PACKAGE_NAME = "com.kakao.talk"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,39 +24,41 @@ class MainActivity : Activity() {
         window?.apply {
             try {
                 addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-                statusBarColor = resources.getColor(R.color.statusBarColor)
+                statusBarColor = this@MainActivity.resources.getColor(R.color.statusBarColor, null)
             } catch (ignored: Throwable) {
             }
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            try {
-                window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            } catch (ignored: Throwable) {
-            }
-        }
-
-        apply.setOnClickListener {
+        val applyButton = findViewById<Button>(R.id.apply)
+        applyButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(KAKAOTALK_SETTINGS_THEME_URI + packageName)
             startActivity(intent)
             finish()
         }
 
-        market.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_URI + KAKAO_TALK_PACKAGE_NAME))
+        val marketButton = findViewById<Button>(R.id.market)
+        marketButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(MARKET_URI + KAKAOTALK_PACKAGE_NAME))
             startActivity(intent)
             finish()
         }
 
-        try {
-            packageManager.getPackageInfo(KAKAO_TALK_PACKAGE_NAME, 0)
-            apply.visibility = View.VISIBLE
-            market.visibility = View.GONE
+        if (isKakaoTalkInstalled()) {
+            applyButton.visibility = View.VISIBLE
+            marketButton.visibility = View.GONE
+        } else {
+            applyButton.visibility = View.GONE
+            marketButton.visibility = View.VISIBLE
+        }
+    }
+
+    open fun isKakaoTalkInstalled(): Boolean {
+        return try {
+            packageManager.getPackageInfo(KAKAOTALK_PACKAGE_NAME, 0)
+            true
         } catch (e: PackageManager.NameNotFoundException) {
-            apply.visibility = View.GONE
-            market.visibility = View.VISIBLE
+            false
         }
     }
 }
